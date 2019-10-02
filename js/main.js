1980c9ea84dbc9fbbe568b0029b0cd7f9efdfaaf
bokibokan <48064598+bokibokan@users.noreply.github.com>
2019-10-02 15:08:47 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+jQuery(document).ready(function($) {
+    $(".js-blog-carousel").owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 5000,
+        margin: 0,
+        nav: true,
+        dots: false,
+        navText: ["<img src='images/left-arrow.png'>", "<img src='images/right-arrow.png'>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        },
+
+    });
+    $(".js-menu-toggle").click(function() {
+        $(this).toggleClass("activated");
+        //on-off
+
+        $(".js-navigation-wrap").toggleClass("navigation-displayed");
+    });
+});
